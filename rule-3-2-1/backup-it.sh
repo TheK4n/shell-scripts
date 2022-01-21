05933735fd7437c5e0a754cda6929bc7ef6ce792
TheK4n <djvlad967891@gmail.com>
2022-01-21 07:03:06 +0300

    ref backup-it.sh

--- rule-3-2-1/backup-it.sh
+++ rule-3-2-1/backup-it.sh
@@ -6,4 +6,4 @@ cur_dir_name="$(basename "$PWD")"
 mkdir -p "$bkp_dir_1" || true
 mkdir -p "$bkp_dir_2" || true
 tar czf - * | gpg -e -R thek4n > "$bkp_dir_1"/"$cur_dir_name".tar.gz.gpg
-cp "$bkp_dir_1"/"$cur_dir_name".tar.gz.gpg "$bkp_dir_2"/"$cur_dir_name".tar.gz.gpg
+cp "$bkp_dir_1"/"$cur_dir_name".tar.gz.gpg "$bkp_dir_2"
