3cad72c2668bf053096c5027a976e50a1caf8f22
TheK4n <djvlad967891@gmail.com>
2022-01-24 09:26:46 +0300

    ref backup-it, change path in init.sh

--- rule-3-2-1/backup-it.sh
+++ rule-3-2-1/backup-it.sh
@@ -1,9 +1,7 @@
 
 bkp_dir_1=~/Backup/1/$(date +"%d-%m-%y")
-bkp_dir_2=~/Backup/2/$(date +"%d-%m-%y")
 cur_dir_name="$(basename "$PWD")"
 
-mkdir -p "$bkp_dir_1" || true
-mkdir -p "$bkp_dir_2" || true
+mkdir -p ~/Backup/{1..2}/$(date +"%d-%m-%y")
 tar czf - * | gpg -e -R thek4n > "$bkp_dir_1"/"$cur_dir_name".tar.gz.gpg
 cp "$bkp_dir_1"/"$cur_dir_name".tar.gz.gpg "$bkp_dir_2"
--- rule-3-2-1/init.sh
+++ rule-3-2-1/init.sh
@@ -1,3 +1,3 @@
-# /run/media/backup - mount point of raid-1
-ln -s /run/media/backup ~/Backup/2 || true
+# /media/backup - mount point of raid-1
+ln -s /media/backup ~/Backup/2 || true
 mkdir -p ~/Backup/1 || true
