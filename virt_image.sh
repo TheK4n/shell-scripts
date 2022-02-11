b8b9a648bdacc8e9b7955ceb2c4ed633b7d682da
TheK4n <djvlad967891@gmail.com>
2022-02-11 10:10:12 +0300

    change virt_image.sh: create tmpfs and create virtual disk into tmpfs and mount into mnt dir

--- virt_image.sh
+++ virt_image.sh
@@ -1,3 +1,6 @@
-dd if=/dev/zero of=test.img bs=1024M count=1
-mkfs.ext4 test.img
-sudo mount -t auto -o loop test.img /mnt
+mkdir tmp mnt
+sudo mount -t tmpfs -o size=2G tmpfs tmp
+
+dd if=/dev/zero of=tmp/test.img bs=1024M count=1
+mkfs.ext4 tmp/test.img
+sudo mount -t ext4 -o loop tmp/test.img mnt
