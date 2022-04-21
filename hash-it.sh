aefd7c60f0362ce312e3ad49471c269ef8e55814
TheK4n <djvlad967891@gmail.com>
2022-04-21 18:56:47 +0300

    add recursive func

--- hash-it.sh
+++ hash-it.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 PEPPER_FILE=~/.hash_it_pepper
+SALT_LEN=64
 
 
 die() {
@@ -20,19 +21,26 @@ verify - read password:hash from stdin retuns result"
 cmd_init() {
     test -e $PEPPER_FILE && die "Already initialized" 1
     umask 0077  # -rw-------
-    date +%s%N | sha256sum | head -c 64 > $PEPPER_FILE
+    date +%s%N | sha256sum | head -c $SALT_LEN > $PEPPER_FILE
 }
 
 cmd_gen_salt() {
-    date +%s%N | sha256sum | head -c 64
+    date +%s%N | sha256sum | head -c $SALT_LEN
 }
 
 get_pepper() {
     cat $PEPPER_FILE
 }
 
-get_prehash() {
-    cat | sha512sum | head -c 128
+recursive_prehash() {
+    password=$1
+    salt=$2
+    pepper=$3
+    pre_hash=$password$salt$pepper
+    for i in {1..150}; do
+        pre_hash="$(echo $pre_hash$salt$pepper | sha512sum | head -c 128)"
+    done
+    echo $pre_hash
 }
 
 cmd_hash_password() {
@@ -42,11 +50,7 @@ cmd_hash_password() {
     salt="$(cmd_gen_salt)"
     pepper="$(get_pepper)"
 
-    pre_hash=$password$salt$pepper
-
-    for i in {1..150}; do
-        pre_hash="$(echo $pre_hash$salt$pepper | get_prehash)"
-    done
+    pre_hash=$(recursive_prehash $password $salt $pepper)
 
     echo "$pre_hash$salt"
 }
@@ -60,14 +64,10 @@ cmd_verify() {
     test -z "$password" && die "No password (password:hash)" 1
     test -z "$prev_hash" && die "No hash (password:hash)" 1
 
-    salt="$(echo "$prev_hash" | tail -c 65)"
+    salt="$(echo "$prev_hash" | tail -c $(($SALT_LEN + 1)))"
     pepper="$(get_pepper)"
 
-    pre_hash=$password$salt$pepper
-
-    for i in {1..150}; do
-        pre_hash="$(echo $pre_hash$salt$pepper | get_prehash)"
-    done
+    pre_hash=$(recursive_prehash $password $salt $pepper)
 
     test "$prev_hash" = "$pre_hash$salt" && echo "Ok" || die "Validation failed" 1
 }
