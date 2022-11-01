7f97493607a19a97ddfcc1d884e944bea094fd63
TheK4n <djvlad967891@gmail.com>
2022-11-01 20:52:25 +0300

    ref

--- del_logs.sh
+++ del_logs.sh
@@ -2,16 +2,4 @@
 
 # removes log files with size greater then 100Mb
 
-for i in $(du -a /var/log/* | sort -hr | tr '\t' ':')
-do
-
-    size_file=( $(echo "$i" | tr ':' ' ') )
-
-    size="${size_file[0]}"
-    file="${size_file[1]}"
-
-    if [ "$size" -ge 102400 ] && [ -f "$file" ]; then
-        rm "$file"
-    fi
-
-done
+find /var/log -size +100M -a -type f 2>/dev/null | xargs rm
